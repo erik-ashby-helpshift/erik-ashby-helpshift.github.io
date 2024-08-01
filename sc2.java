ade203cedb4abfd678e413aa86abbf4830f2cb53
erik-ashby-helpshift <64989981+erik-ashby-helpshift@users.noreply.github.com>
2024-08-01 12:01:54 -0600

    Update sc2.java

--- sc2.java
+++ sc2.java
@@ -1,1 +1,30 @@
 
+<!DOCTYPE html>
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <title></title>
+   <script>
+(function () {
+    var PLATFORM_ID = "ashbys_platform_20210525002032362-e6b600a154a87ab",
+    DOMAIN = "ashbys",
+    LANGUAGE = "en";
+
+    window.helpshiftConfig = {
+      platformId: PLATFORM_ID,
+      domain: DOMAIN,
+      language: LANGUAGE
+    };
+  }) ();
+
+  !function(t,e){if("function"!=typeof window.Helpshift){var n=function(){n.q.push(arguments)};n.q=[],window.Helpshift=n;var i,a=t.getElementsByTagName("script")[0];if(t.getElementById(e))return;i=t.createElement("script"),i.async=!0,i.id=e,i.src="https://webchat.helpshift.com/latest/webChat.js";var o=function(){window.Helpshift("init")};window.attachEvent?i.attachEvent("onload",o):i.addEventListener("load",o,!1),a.parentNode.insertBefore(i,a)}else window.Helpshift("update")}(document,"hs-chat");
+  Helpshift("open");
+   </script>
+
+</head>
+
+<body>
+
+</body>
+</html>
